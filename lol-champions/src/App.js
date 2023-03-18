96e280cd11986da4b234d322d347c24a732ac5a8
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-03-18 18:54:36 +0300

    feat:create home page

---
+++ lol-champions/src/App.js
@@ -0,0 +1,13 @@
+import Champions from './components/champions';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Champions</h1>
+      <Champions/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lol-champions/src/components/champions/index.js
@@ -0,0 +1,29 @@
+import React, { useState , useEffect } from 'react'
+import axios from 'axios'
+import './index.css'
+
+function Champions() {
+   const [champions,setChampions] = useState([]);
+   useEffect(() => {
+           axios("https://ddragon.leagueoflegends.com/cdn/11.14.1/data/en_US/champion.json")
+            .then(data => data.data)
+            .then((data) => {
+                console.log(data);
+                setChampions(Object.keys(data.data));
+            })
+            .catch(eror => console.log(eror))
+   }, [])
+   return (
+    <div className='container'>
+        
+        {
+            champions.map((champion,index) => (
+                <div className='foto' key={index}><img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion}_0.jpg`} alt={champion} /><div className='name'>{champion}</div></div>
+            ))
+        }
+        
+    </div>
+);
+}
+
+export default Champions
