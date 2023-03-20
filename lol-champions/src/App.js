a59c981405057e98635a1d7e6030620561abe3b4
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-03-20 14:09:46 +0300

    feat:desing

--- lol-champions/src/App.js
+++ lol-champions/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Champions</h1>
+      <h1>League Of Legends Champions</h1>
       <Champions/>
     </div>
   );
--- lol-champions/src/components/champions/index.js
+++ lol-champions/src/components/champions/index.js
@@ -8,17 +8,18 @@ function Champions() {
            axios("https://ddragon.leagueoflegends.com/cdn/11.14.1/data/en_US/champion.json")
             .then(data => data.data)
             .then((data) => {
-                console.log(data);
+                console.log(data.data);
                 setChampions(Object.keys(data.data));
             })
             .catch(eror => console.log(eror))
    }, [])
+   
    return (
     <div className='container'>
         
         {
             champions.map((champion,index) => (
-                <div className='foto' key={index}><img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion}_0.jpg`} alt={champion} /><div className='name'>{champion}</div></div>
+                <div className='foto'key={index}><img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion}_0.jpg`} alt={champion} /><div className='name'>{champion}</div></div>
             ))
         }
         
