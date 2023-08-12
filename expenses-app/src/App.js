8bdbb90de9256bd3c82e03533ae0987bd51d64b7
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-08-12 21:42:59 +0300

    feat:Create ExpenseItem.css

--- expenses-app/src/App.js
+++ expenses-app/src/App.js
@@ -1,11 +1,10 @@
 import ExpenseItem from "./components/ExpenseItem";
 
-
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem/>
+      <ExpenseItem />
     </div>
   );
 }
--- expenses-app/src/components/ExpenseItem.js
+++ expenses-app/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        <div className="expense-item">
+            <div>Date</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
