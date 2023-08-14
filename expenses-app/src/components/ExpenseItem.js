d5fa2d4b5cfb08c4951f58297f56c6428b25c6f8
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-08-14 13:40:55 +0300

    first homework done

--- expenses-app/src/components/ExpenseItem.js
+++ expenses-app/src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <ExpenseDate date ={props.date} />
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
