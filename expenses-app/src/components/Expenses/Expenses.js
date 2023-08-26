5f8f643cd34d8dc5a08b72eb5393bb2240525a13
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-08-26 12:52:05 +0300

    feat:add a item

--- expenses-app/src/components/Expenses/Expenses.js
+++ expenses-app/src/components/Expenses/Expenses.js
@@ -4,35 +4,25 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 function Expenses(props) {
-    const [filteredYear, setFilteredYear] = useState('2020');
+    const [filteredYear, setFilteredYear] = useState("2020");
     const filterExpensesHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
     return (
         <div>
             <Card className='expenses'>
-                <ExpensesFilter selected = {filteredYear} onFilterExpenses={filterExpensesHandler} />
-            
-                <ExpenseItem
-                    title={props.expenses[0].title}
-                    amount={props.expenses[0].amount}
-                    date={props.expenses[0].date}
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onFilterExpenses={filterExpensesHandler}
                 />
-                <ExpenseItem
-                    title={props.expenses[1].title}
-                    amount={props.expenses[1].amount}
-                    date={props.expenses[1].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[2].title}
-                    amount={props.expenses[2].amount}
-                    date={props.expenses[2].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[3].title}
-                    amount={props.expenses[3].amount}
-                    date={props.expenses[3].date}
-                />
+                {props.expenses.map((expense,key) => (
+                    <ExpenseItem
+                        key = {key}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
             </Card>
         </div>
     );
