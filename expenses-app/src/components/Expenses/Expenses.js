8f70cd2ab317e467d303a8cbe3b3cb82a4b1213e
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-08-26 15:11:49 +0300

    feat:create ExpensesList component

--- expenses-app/src/components/Expenses/Expenses.js
+++ expenses-app/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterExpensesHandler = (selectedYear) => {
@@ -19,19 +19,7 @@ function Expenses(props) {
           selected={filteredYear}
           onFilterExpenses={filterExpensesHandler}
         />
-        {filteredByYearItem.length === 0 && <p>No expenses found.</p>}
-        {filteredByYearItem.length === 0 ? (
-          <p>No expenses found.</p>
-        ) : (
-          filteredByYearItem.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))
-        )}
+        <ExpensesList items ={filteredByYearItem}/>
       </Card>
     </div>
   );
