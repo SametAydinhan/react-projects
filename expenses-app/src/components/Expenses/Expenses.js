f9cb005827650042276b37442df569c6a2a9e77e
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-08-26 14:06:50 +0300

    feat: Outputting conditional Content

--- expenses-app/src/components/Expenses/Expenses.js
+++ expenses-app/src/components/Expenses/Expenses.js
@@ -4,30 +4,37 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 function Expenses(props) {
-    const [filteredYear, setFilteredYear] = useState("2020");
-    const filterExpensesHandler = (selectedYear) => {
-        setFilteredYear(selectedYear);
-    };
-    const filteredByYearItem = props.expenses.filter(expense => expense.date.getFullYear().toString() === filteredYear);
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterExpensesHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredByYearItem = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
 
-    return (
-        <div>
-            <Card className='expenses'>
-                <ExpensesFilter
-                    selected={filteredYear}
-                    onFilterExpenses={filterExpensesHandler}
-                />
-                {filteredByYearItem.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
-            </Card>
-        </div>
-    );
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterExpenses={filterExpensesHandler}
+        />
+        {filteredByYearItem.length === 0 && <p>No expenses found.</p>}
+        {filteredByYearItem.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredByYearItem.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
+      </Card>
+    </div>
+  );
 }
 
 export default Expenses;
