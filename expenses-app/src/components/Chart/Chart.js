43990dac86d79a4ad0ba2dfb90b610c8a27b477f
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-08-26 16:43:45 +0300

    feat:create a chart and chartbar

---
+++ expenses-app/src/components/Chart/Chart.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totolMaximum = Math.max(...dataPointValues);
+    return (
+        <div className='chart'>
+            {props.dataPoints.map((dataPoint) => (
+                <ChartBar
+                    key={dataPoint.label}
+                    value={dataPoint.value}
+                    maxValue={totolMaximum}
+                    label={dataPoint.label}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Chart;
--- expenses-app/src/components/Expenses/Expenses.js
+++ expenses-app/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterExpensesHandler = (selectedYear) => {
@@ -19,6 +20,7 @@ function Expenses(props) {
           selected={filteredYear}
           onFilterExpenses={filterExpensesHandler}
         />
+        <ExpensesChart expenses= {filteredByYearItem}/>
         <ExpensesList items ={filteredByYearItem}/>
       </Card>
     </div>
