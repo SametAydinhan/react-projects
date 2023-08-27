d4ddf440a1281ad90631dae14336d7c05a988d15
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-08-27 15:28:31 +0300

    Learn a styled-components

--- course-goals/src/components/CourseGoals/CourseInput/CourseInput.js
+++ course-goals/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,16 +25,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
-        <input
-          style={{
-            borderColor: !isValid ? "red" : "#ccc",
-            background: !isValid ? "salmon" : "transparent",
-          }}
-          type='text'
-          onChange={goalInputChangeHandler}
-        />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type='text' onChange={goalInputChangeHandler} />
       </div>
       <Button type='submit'>Add Goal</Button>
     </form>
