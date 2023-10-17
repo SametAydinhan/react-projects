057f3f4eefca64848b087e8936e7e1c82de2870b
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-10-17 16:21:45 +0300

    feat:add the cart button

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import {Fragment} from 'react';
 import mealsImage from '../../assets/meals.jpg'
 import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = props => {
     return <Fragment>
         <header className={classes.header}>
             <h1>MealsForYou</h1>
-            <button>Cart</button>
+            <HeaderCartButton/>
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt='A table full of delicious food!'/>
