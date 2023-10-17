e89b17b97b1749cbca5197470e3c6ed801737e93
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-10-17 16:39:26 +0300

    feat:add meals components

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -1,18 +1,20 @@
-import {Fragment} from 'react';
-import mealsImage from '../../assets/meals.jpg'
-import classes from './Header.module.css'
-import HeaderCartButton from './HeaderCartButton';
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
-const Header = props => {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>MealsForYou</h1>
-            <HeaderCartButton/>
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt='A table full of delicious food!'/>
-        </div>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>MealsForYou</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
     </Fragment>
+  );
 };
 
-export default Header
+export default Header;
---
+++ food-order-app/src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import MelasSummary from "./MealsSummary";
+import AvailavleMeals from "./AvailableMeals";
+
+const Meals = () => {
+ return <Fragment>
+    <MelasSummary />
+    <AvailavleMeals />
+ </Fragment>
+}
+
+export default Meals;
